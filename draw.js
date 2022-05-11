0ffa2188b9ae19b3f06b8809f36b4f2b03b5c711
consolelogreece <consolelogreece@gmail.com>
2022-05-11 15:38:27 +0100

    fixed some weird bug that made it not work with numbers that werent powers of 2

--- draw.js
+++ draw.js
@@ -1,40 +1,50 @@
-
-var fragShadersrc = `#version 300 es
+var fragShadersrc = (
+`#version 300 es
 precision mediump float;
 out vec4 fragColor;
+in vec4 v_colour;
+in float v_index;
 
-uniform sampler2D particles;
 
 void main(){
-    fragColor = vec4(1,0,0,1);
-}
-`
+    //vec4 colour = vec4(v_index / 17.0 ,0,0,1);
 
+    fragColor = v_colour;
+}
+`);
 
 var vertShadersrc = `#version 300 es
+
 uniform sampler2D particles;
+uniform float nParticleDimensions;
+
 in float a_positionIndex;
+out float v_index;
+out vec4 v_colour;
 
 void main() {
    
-    float  resolution = 4.0;
     // Fetch the pixel from the texture using the position index. As the index is a number, and the texture is basically a 2D array,
     // We have to calclate what the x and y position of the pixel in the array is based on that 1D index.
-    vec4 colour = texture(particles, vec2(fract(a_positionIndex / resolution), floor(a_positionIndex / resolution) / resolution));
+    vec4 colour = texture(particles, vec2(fract(a_positionIndex / nParticleDimensions), floor(a_positionIndex / nParticleDimensions) / nParticleDimensions));
+
+    v_index = a_positionIndex;
+
+    v_colour = colour;
 
     // Calculate the x and y positions by combining the rg and ba values for x and y respectively.
-    vec2 pos = vec2(colour.r / 255.0 + colour.b,colour.g / 255.0 + colour.a);
+    vec2 pos = vec2(colour.r / 255.0 + colour.b, colour.g / 255.0 + colour.a);
 
     // color.x and color.y can only be positive, so in order to convert them into clip space we double it subtract it from 1 so that it can be anywhere on screen.
-    float x = 1.0 - pos.x * 2.0;
-    float y = 1.0 - pos.y * 2.0;
+    float x = (pos.x * 2.0) - 1.0;
+    float y = 1.0 - 2.0 * (pos.y);
 
-    gl_PointSize = 2.0;
-    gl_Position = vec4(x,y, 0.0, 1.0);
+    gl_PointSize = 20.0;
+    gl_Position = vec4( x,y, 0, 1);
 }
 `
 
-var gl = document.getElementById("c").getContext("webgl2");
+var gl = document.getElementById("c").getContext("webgl2", {preserveDrawingBuffer: false});
 var vertShader = utils.createShader(gl, gl.VERTEX_SHADER, vertShadersrc);
 var fragShader = utils.createShader(gl, gl.FRAGMENT_SHADER, fragShadersrc);
 var drawProgram = utils.createProgram(gl, vertShader, fragShader);
--- update.js
+++ update.js
@@ -1,38 +1,60 @@
+var fragShadersrc = (
+`#version 300 es
 
-var fragShadersrc = `#version 300 es
 precision mediump float;
-out vec4 fragColor;
 
 uniform sampler2D particles;
+
+out vec4 fragColor;
+
 in vec2 v_texturePosition;
+
+float rand(const vec2 co) {
+    float t = dot(vec2(12.9898, 78.233), co);
+    return fract(sin(t) * (4375.85453 + t));
+}
 
 void main(){
     // get the pixel colour at the given position
     vec4 colour = texture(particles, v_texturePosition);
 
     // find the x and y values from the colour. x = r + g, y = b + a.
-    vec2 pos = vec2(colour.r / 255.0 + colour.b,colour.g / 255.0 + colour.a);
+    // positions range from 0 to 1. to recode, we will have to remultiply by 255 wherever necessary.
+    vec2 pos = vec2(colour.r / 255.0 + colour.b, colour.g / 255.0 + colour.a);
 
     // Do whatever we want to update the position of the particle.
-    pos.x+= 0.0013;
-        
+    pos.y += 0.0001;
+
+    // float random = rand(pos * v_texturePosition);
+
+
+    // if (random > 0.99) {
+    //     pos.x = rand(vec2(random, v_texturePosition));
+    //     pos.y = rand(vec2(pos.x, colour.a));
+    // }
+
+   //colour.r += 0.2;
+    
     // convert the positions back in to a colour and write them to the texture (this will be texture2, and not the default texture(canvas)).
-    fragColor = vec4(fract(pos * 255.0),floor(pos * 255.0) / 255.0);
+    fragColor = vec4(fract(pos * 255.0), floor(pos * 255.0) / 255.0);
 }
-`
 
 
-var vertShadersrc = `#version 300 es
+
+`);
+
+var vertShadersrc = (
+`#version 300 es
 in vec2 a_xycoord;
 out vec2 v_texturePosition;
 
 void main() {
-    v_texturePosition = a_xycoord;
+    v_texturePosition = (1.0 + a_xycoord) / 2.0;
 
-    // xy_coord had to be normalized from 0 to 1, to -1 to 1 in order fit properly in clip space.
-    gl_Position = vec4(1.0 - 2.0 * a_xycoord, 0, 1);
+    // xy_coord had to be normalized from 0 to 1, to -1 to 1 in order fit properly in clip space. <- NOT ACTUALLY WAHT IS HAPPENING HERE
+    gl_Position = vec4(a_xycoord, 0,1);
 }
-`
+`);
 
 var gl = document.getElementById("c").getContext("webgl2");
 var vertShader = utils.createShader(gl, gl.VERTEX_SHADER, vertShadersrc);
